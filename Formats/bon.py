097234b6db5fbaced06ce3fea6d4a0d3a8b7d92c
aspadm <asp-adm@yandex.ru>
2018-08-13 21:49:25 +0300

    Not write .yaml if .bon is .res

--- Formats/bon.py
+++ Formats/bon.py
@@ -5,11 +5,10 @@ from binary_readers import *
 def build_yaml(info):
     buf = ""
 
-    if info != None:
-        for value in info:
-            buf += "- x: {:}\n  y: {:}\n  z: {:}\n".format(value[0],
-                                                           value[1],
-                                                           value[2])
+    for value in info:
+        buf += "- x: {:}\n  y: {:}\n  z: {:}\n".format(value[0],
+                                                       value[1],
+                                                       value[2])
     
     return buf
 
@@ -17,7 +16,7 @@ def read_info(file_name):
     info = []
     with open(file_name, "rb") as file:
         if file.read(4) == b'\x3C\xE2\x9C\x01':
-            print("Unpack as res first")
+            print("Unpack as res")
             file.seek(0)
 
             from res import read_filetree, unpack_res
@@ -39,10 +38,11 @@ if __name__ == '__main__':
     if 2 <= len(sys.argv) <= 3:
         info = read_info(sys.argv[1])
         
-        if len(sys.argv) == 2:
-            print(build_yaml(info))
-        else:
-            with open(sys.argv[2], "w") as file:
-                file.write(build_yaml(info))
+        if info != None:
+            if len(sys.argv) == 2:
+                print(build_yaml(info))
+            else:
+                with open(sys.argv[2], "w") as file:
+                    file.write(build_yaml(info))
     else:
         print("Usage: bon.py input.bon [output.yaml]")
