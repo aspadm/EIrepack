045597c4b5985f925ed0a1e3f4c6015fa66a4ee2
aspadm <asp-adm@yandex.ru>
2018-08-16 14:05:26 +0300

    Read time from .cam

--- Formats/cam.py
+++ Formats/cam.py
@@ -5,15 +5,16 @@ from binary_readers import *
 def build_cams(cams):
     buf = ""
     for cam in cams:
-        buf += "- position:\n"
-        buf += "    x: {}\n    y: {}\n    z: {}\n".format(cam[0][0],
-                                                          cam[0][1],
-                                                          cam[0][2])
+        buf += "- time: {}\n".format(cam[0])
+        buf += "  position:\n"
+        buf += "    x: {}\n    y: {}\n    z: {}\n".format(cam[1][0],
+                                                          cam[1][1],
+                                                          cam[1][2])
         buf += "  rotation:\n"
-        buf += "    w: {}\n    x: {}\n    y: {}\n    z: {}\n".format(cam[1][0],
-                                                                     cam[1][1],
-                                                                     cam[1][2],
-                                                                     cam[1][3])
+        buf += "    w: {}\n    x: {}\n    y: {}\n    z: {}\n".format(cam[2][0],
+                                                                     cam[2][1],
+                                                                     cam[2][2],
+                                                                     cam[2][3])
 
     return buf
 
@@ -21,9 +22,9 @@ def read_cams(f_name):
     with open(f_name, "rb") as file:
         cams = []
         for i in range(os.path.getsize(f_name) // 36):
-            read_uint(file)
-            read_uint(file)
             cams.append([])
+            cams[i].append(read_uint(file))
+            read_uint(file)
             cams[i].append(read_float(file, 3))
             cams[i].append(read_float(file, 4))
 
