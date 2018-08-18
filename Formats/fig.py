491b7827b30fa15974164d0955d01f9a4e493173
aspadm <asp-adm@yandex.ru>
2018-08-18 21:50:05 +0300

    Add .fig support

---
+++ Formats/fig.py
@@ -0,0 +1,110 @@
+import sys
+from binary_readers import *
+
+keys = ["blocks", "vertex_count", "normal_count", "texcoord_count",
+        "index_count", "vertex_component_count", "morph_component_count",
+        "group", "texture_number", "center", "min", "max", "radius",
+        "vertices", "normals", "texcoords", "indexes", "vertex_components",
+        "morph_components"]
+
+def build_yaml(info):
+    buf = ""
+
+    for i in range(len(info)):
+        buf += keys[i] + ":"
+        if type(info[i]) != list:
+            buf += " " + str(info[i]) + "\n"
+        else:
+            buf += "\n"
+            if i in [9, 10, 11]:
+                for xyz in info[i]:
+                    buf += "  - x: {}\n    y: {}\n    z: {}\n".format(xyz[0],
+                                                                      xyz[1],
+                                                                      xyz[2])
+            elif i == 12:
+                for obj in info[i]:
+                    buf += "  - " + str(obj) + "\n"
+            elif i == 13:
+                for n in range(info[0]):
+                    buf += "  - variant{}:\n".format(n)
+                    for xyz in info[i]:
+                        buf += "      - x:\n          - {}\n          \
+- {}\n          - {}\n          - {}\n".format(xyz[0][n][0], xyz[0][n][1],
+                                               xyz[0][n][2], xyz[0][n][3])
+                        buf += "        y:\n          - {}\n          \
+- {}\n          - {}\n          - {}\n".format(xyz[1][n][0], xyz[1][n][1],
+                                               xyz[1][n][2], xyz[1][n][3])
+                        buf += "        z:\n          - {}\n          \
+- {}\n          - {}\n          - {}\n".format(xyz[2][n][0], xyz[2][n][1],
+                                               xyz[2][n][2], xyz[2][n][3])
+            elif i == 14:
+                for xyz in info[i]:
+                    buf += "  - x:\n      - {}\n      - {}\n      - {}\n\
+      - {}\n    y:\n      - {}\n      - {}\n      - {}\n      - {}\n\
+    z:\n      - {}\n      - {}\n      - {}\n      - {}\n".format(
+                                xyz[0][0], xyz[0][1], xyz[0][2], xyz[0][3],
+                                xyz[1][0], xyz[1][1], xyz[1][2], xyz[1][3],
+                                xyz[2][0], xyz[2][1], xyz[2][2], xyz[2][3])
+            elif i == 15:
+                for uv in info[i]:
+                    buf += "  - u: {}\n    v: {}\n".format(uv[0], uv[1])
+            elif i == 16:
+                for v3 in info[i]:
+                    buf += "  - - {}\n    - {}\n    - {}\n".format(v3[0],
+                                                                   v3[1],
+                                                                   v3[2])
+            elif i == 17:
+                for v3 in info[i]:
+                    buf += "  - v: {}\n    n: {}\n    t: {}\n".format(v3[0],
+                                                                      v3[1],
+                                                                      v3[2])
+            elif i == 18:
+                for v2 in info[i]:
+                    buf += "  - m: {}\n    v: {}\n".format(v2[0], v2[1])
+            else:
+                print("WARN!__________", i)
+    
+    return buf
+
+def read_info(file_name):
+    info = []
+    with open(file_name, "rb") as file:
+        if file.read(3) != b'\x46\x49\x47':
+            print("Incorrect magic!")
+            return
+
+        n = read_byte(file) - 48
+        info.append(n)
+        info.extend(read_uint(file, 6))
+        read_uint(file)
+        info.extend(read_uint(file, 2))
+        info[4] //= 3
+                
+        for i in range(3):
+            info.append([read_float(file, 3) for j in range(n)])
+
+        info.append(read_float(file, n))
+
+        info.append([[[read_float(file, 4) for j in range(n)] for xyz in range(3)] for i in range(info[1])])
+        info.append([[read_float(file, 4) for xyzw in range(4)] for i in range(info[2])])
+
+        info.append([read_float(file, 2) for j in range(info[3])])
+        info.append([read_ushort(file, 3) for i in range(info[4])])
+
+        info.append([read_ushort(file, 3) for i in range(info[5])])
+        info.append([read_ushort(file, 2) for i in range(info[6])])
+
+    return info
+
+if __name__ == '__main__':
+    if 2 <= len(sys.argv) <= 3:
+        info = read_info(sys.argv[1])
+        
+        if info != None:
+            if len(sys.argv) == 2:
+                print(build_yaml(info))
+            else:
+                with open(sys.argv[2], "w") as file:
+                    file.write(build_yaml(info))
+    else:
+        print("Usage: fig.py input.fig [output.yaml]")
